737b3aff1c28e7f33836927846d09b85d38aac0e
Felipe Cantagalli <felipe.cantagalli@gmail.com>
2020-01-06 23:40:18 -0300

    change to doctorname

--- src/api/chat/medicalStructureService.js
+++ src/api/chat/medicalStructureService.js
@@ -14,12 +14,15 @@ export default class MedicalStructureService {
      * @returns {Promise<void>} save and process result
      */
     saveAndProcess(message, senderId) {
+        console.log("AQUIIII")
+        console.log(message)
+        console.log(senderId)
         let option = {
             method: 'POST',
             uri: this._baseUrl + "/processText",
             body: {
                 body: message,
-                sender_id: senderId
+                doctorName: senderId
             },
             json: true
         }
@@ -55,7 +58,6 @@ export default class MedicalStructureService {
                 "name": name
             }
         }
-        console.log("CHEGOU AQUI!!!")
         return request.post(option)
     }
     
